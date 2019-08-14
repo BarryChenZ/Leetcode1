977e62aa8f020e583808fffd6a4c8c10214c904e
BarryChenZ <43691684+BarryChenZ@users.noreply.github.com>
2019-08-14 20:15:50 +0800

    Create P142_LinkedListCycle_II.cpp

---
+++ P142_LinkedListCycle_II.cpp
@@ -0,0 +1,24 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode *detectCycle(ListNode *head) {
+        ListNode* slow = head;
+        ListNode* fast = head;
+        while(fast && fast->next){
+            slow = slow->next;
+            fast = fast->next->next;
+            if(fast == slow){
+                for(;head != slow;head = head->next,slow = slow->next);
+                return head;
+            }
+        }
+        return NULL;
+    }
+};
